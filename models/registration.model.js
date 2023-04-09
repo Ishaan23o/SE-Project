2d7c56e990a1c8c1938d2e4e3ef3221d3f140973
prathambhatia86 <85384335+prathambhatia86@users.noreply.github.com>
2023-04-09 23:10:31 +0530

    implemented registration schema

---
+++ models/registration.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://127.0.0.1:27017/se_project")
+    .then(() => {
+        console.log("connection established")
+    })
+    .catch(() => {
+        console.log("failed to connect");
+    })
+const registration_schema = new mongoose.Schema({
+    Event_ID:
+    {
+        type: String,
+        required: true
+    },
+   registered:
+   {
+    type:{},
+    required:true
+   }
+}
+)
+const collection = new mongoose.model("registrations", registration_schema)
+module.exports = collection
