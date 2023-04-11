0bb7f7f4ad1991ad0b1b220949af89ce3c6c2069
prathambhatia86 <85384335+prathambhatia86@users.noreply.github.com>
2023-04-12 01:50:23 +0530

    implemented real time email services(commented out)

--- controllers/registrations.controller.js
+++ controllers/registrations.controller.js
@@ -1,5 +1,7 @@
 const event_collection = require("../models/events.model")
 const registration_collection = require("../models/registration.model")
+const nodemailer = require("nodemailer");
+const mailgen=require("mailgen");
 async function register(req, res) {
   try{
     let find_x = await registration_collection.findOne({ 'Event_ID': req.body.register });
@@ -9,6 +11,33 @@ async function register(req, res) {
     await registration_collection.updateOne({ 'Event_ID': req.body.register }, find_x, { $upsert: false }, function (err, result) {
       if (err) return res.send(500, { error: err });
     });
+    // let config={
+    //   service:'gmail',
+    //   auth:{
+    //     user:'prathambhatia8686@gmail.com',
+    //     pass:'urgcylkulebavygd'
+    //   }
+    // }
+    // let transporter=nodemailer.createTransport(config);
+    // let mail_generator=new mailgen({
+    //   theme:"default",
+    //   product:{
+    //     name: "Mailgen",
+    //     link:'https://mailgen.js'
+    //   }
+    // })
+    // let response={
+    //   body:{
+    //     intro:"you have successfully registered",
+    //   }
+    // }
+    // let mail=mail_generator.generate(response)
+    // let message={
+    //   from:'prathambhatia8686@gmail.com',
+    //   to:cur_session,
+    //   html:mail
+    // }
+    // transporter.sendMail(message);
     let find_elem = await event_collection.find({"scope.scope":"public"});
     res.render("find_event", { data: find_elem });}
     catch{
